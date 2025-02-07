c56d94345c182b4c482731078d3cfe96cdc4d69c
titan <titan@titans-MacBook-Pro.local>
2025-02-07 04:02:47 +0100

    profile ui

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,10 +1,35 @@
 import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function MyProfile(){
     return (
         <>
             <Header />
-            <div>Profile</div>
+            <main>
+                <div className="container">
+                    <div className="grid grid-cols-12 gap-x-6">
+                        <aside className="col-span-3">
+                            <nav className="mt-8">
+                                <ul className="list-profile-nav">
+                                    <li><Link href="/myprofile" className="font-bold">Profil</Link></li>
+                                    <li><Link href="/settings">Ustawienia</Link></li>
+                                    <li><Link href="/dataexport">Eksport danych</Link></li>
+                                </ul>
+                            </nav>
+                        </aside>     
+                        <section className="col-span-9">
+                            <h1 className="h3 mt-4 mb-20 block">Profil</h1>
+                            <div className="flex">
+                                <img src={null} alt="" className="w-[64px] h-[64px] bg-gray-100 rounded-full" />
+                                <div className="ms-6">
+                                    <h2 className="h5 !mb-1">Allegro</h2>
+                                    <p className="text-sm text-gray-500">Warszawa, Mazowieckie</p>
+                                </div>
+                            </div>
+                        </section> 
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image';
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 
 export default function Header() {
   return (
     <header className="py-2 mb-6 border-b">
         <div className="container grid grid-cols-12 gap-x-6">
-            <div className="col-span-6 lg:col-span-9">
+            <div className="col-span-6 lg:col-span-8">
                 <Link href="/" className="mt-3 inline-block me-8 font-semibold">IT</Link>
                 <nav className="inline-block mt-3">
                   <ul>
@@ -14,9 +23,18 @@ export default function Header() {
                   </ul>
                 </nav>
             </div>
-            <div className="col-span-3">
-              <Link href="/login" className="me-4">Zaloguj się</Link>
-              <Link href="/signup" className="btn btn-primary">Dodaj ogłoszenie</Link>
+            <div className="col-span-4">
+              <div className="flex justify-end w-full">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="me-4">Twoje konto</DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem><Link href="/myprofile">Profil</Link></DropdownMenuItem>
+                    <DropdownMenuItem><Link href="/settings">Ustawienia</Link></DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                {/* <Link href="/login" className="me-4">Zaloguj się</Link> */}
+                <Link href="/signup" className="btn btn-primary">Dodaj ogłoszenie</Link>
+              </div>
             </div>
         </div>
     </header>
