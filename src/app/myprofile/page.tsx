31cac4e7e7cdb4ede97fc971b220f3ad16496151
titan <titan@titans-MacBook-Pro.local>
2025-02-10 01:06:24 +0100

    UI page profile

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+
 import Header from "@/components/Header";
 import Link from "next/link";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Select from "react-select";
+
+type select_option = {
+    value: string,
+    label: string
+}
 
 export default function MyProfile(){
+    const cities: select_option[] = [
+        {value: "warszawa", label: "Warszawa"},
+        {value: "krakow", label: "Kraków"}
+    ];
+
     return (
         <>
             <Header />
@@ -12,19 +27,92 @@ export default function MyProfile(){
                             <nav className="mt-8">
                                 <ul className="list-profile-nav">
                                     <li><Link href="/myprofile" className="font-bold">Profil</Link></li>
+                                    <li><Link href="/payments">Płatności</Link></li>
                                     <li><Link href="/settings">Ustawienia</Link></li>
                                     <li><Link href="/dataexport">Eksport danych</Link></li>
                                 </ul>
                             </nav>
                         </aside>     
                         <section className="col-span-9">
-                            <h1 className="h3 mt-4 mb-20 block">Profil</h1>
-                            <div className="flex">
-                                <img src={null} alt="" className="w-[64px] h-[64px] bg-gray-100 rounded-full" />
+                            <h1 className="h2 mt-4 block">Profil</h1>
+                            <div className="flex py-8">
+                                <img src={undefined} alt="" className="w-[64px] h-[64px] bg-gray-100 rounded-full" />
                                 <div className="ms-6">
-                                    <h2 className="h5 !mb-1">Allegro</h2>
+                                    <h3 className="h5 !mb-1">Allegro</h3>
                                     <p className="text-sm text-gray-500">Warszawa, Mazowieckie</p>
                                 </div>
+                            </div>
+                            <div className="mb-12">
+                                <div className="mb-4">
+                                    <p className="mb-2">Adres email</p>
+                                    <p className="font-semibold">allegro@allegro.pl</p>
+                                </div>
+                                <form action="">
+                                    <div className="mb-3">
+                                        <label className="w-full mb-2 block text-black">Nazwa firmy</label>
+                                        <input className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="w-full mb-2 block text-black">Miejscowość</label>
+                                        <Select
+                                            className="focus:outline-none focus:border-black"
+                                            classNamePrefix="select"
+                                            isClearable
+                                            isSearchable
+                                            placeholder={""}
+                                            name="city_id"
+                                            options={cities}
+                                            onChange={(e) => {}} 
+                                        />
+                                    </div>
+                                    <div className="col-span-full mt-4">
+                                        <button className="bg-black text-white py-3 px-8 font-semibold" type="submit">Zapisz</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div>
+                                <h2 className="h5">Dane do płatności</h2>
+
+                                <form action="">
+                                    <div className="grid grid-cols-12 gap-x-6">
+                                        <div className="col-span-full mb-3">
+                                            <label className="w-full mb-2 block text-black">Nazwa firmy</label>
+                                            <input type="text" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-6 mb-3">
+                                            <label className="w-full mb-2 block text-black">Imię</label>
+                                            <input type="text" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-6 mb-3">
+                                            <label className="w-full mb-2 block text-black">Nazwisko</label>
+                                            <input type="text" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full mb-3">
+                                            <label className="w-full mb-2 block text-black">Adres siedziby</label>
+                                            <input type="text" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-3 mb-3">
+                                            <label className="w-full mb-2 block text-black">Kod pocztowy</label>
+                                            <input type="number" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-9 mb-3">
+                                            <label className="w-full mb-2 block text-black">Miejscowość</label>
+                                            <input type="text" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-6 mb-3">
+                                            <label className="w-full mb-2 block text-black">NIP</label>
+                                            <input type="number" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full md:col-span-6 mb-3">
+                                            <label className="w-full mb-2 block text-black">REGON</label>
+                                            <input type="number" className="w-full border border-2 border-black py-[10px] px-3 focus:outline-none focus:border-black" placeholder="" />
+                                        </div>
+                                        <div className="col-span-full mt-4">
+                                            <button className="bg-black text-white py-3 px-8 font-semibold" type="submit">Zapisz</button>
+                                        </div>
+                                    </div>
+                                    
+                                </form>
                             </div>
                         </section> 
                     </div>
