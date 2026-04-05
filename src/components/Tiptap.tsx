a2c5721aa91db85b407f1cbd8ced3ffc86ef8847
titan <damiankomonski@gmail.com>
2026-04-05 19:04:17 +0200

    wyiswg editor edit, add announcement page

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -4,38 +4,59 @@
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
+import type { Editor } from '@tiptap/core'
 
-const MenuBar = ({ editor }: {editor: any}) => {
+const MenuBar = ({ editor }: {editor: Editor | null}) => {
   if (!editor) {
     return null
   }
 
   return (
-    <div className="control-group">
+    <div className="control-group py-2 px-1 border-b-2 border-black">
       <div className="button-group">
-        <button
+        {/* <button
+          type="button"
           onClick={() => editor.chain().focus().setParagraph().run()}
           className={editor.isActive('paragraph') ? 'is-active' : ''}
         >
           Paragraph
-        </button>
+        </button> */}
+        Wstecz undo
+        Do przodu redo
         <button
-          onClick={(e) => {e.preventDefault(); editor.chain().focus().toggleBold().run()}}
-          className={editor.isActive('bold') ? 'is-active' : ''}
+          type="button"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active mx-2' : 'mx-2'}
         >
           Bold
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive('italic') ? 'is-active' : ''}
+          className={editor.isActive('italic') ? 'is-active mx-2' : 'mx-2'}
         >
           Italic
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive('strike') ? 'is-active' : ''}
+          className={editor.isActive('strike') ? 'is-active mx-2' : 'mx-2'}
         >
           Strike
+        </button>
+        <button
+          type="button" 
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active mx-2' : 'mx-2'}
+        >
+          Bullet list
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          Ordered list
         </button>
       </div>
     </div>
@@ -45,21 +66,23 @@ const MenuBar = ({ editor }: {editor: any}) => {
 export default () => {
   const editor = useEditor({
     extensions: [
-      StarterKit
+      StarterKit.configure({
+        undoRedo: false
+      })
     ],
     content: ``, 
     immediatelyRender: false,
     editorProps: {
         attributes: {
-            class: 'border-2 border-black p-3 min-h-[320px] focus:outline-none',
+            class: 'p-3 max-h-[360px] overflow-scroll focus:outline-none',
         },
     },
   })
 
   return (
-    <>
+    <div className="border-2 border-black rounded-[8px]">
       <MenuBar editor={editor} />
       <EditorContent editor={editor} />
-    </>
+    </div>
   )
 }
